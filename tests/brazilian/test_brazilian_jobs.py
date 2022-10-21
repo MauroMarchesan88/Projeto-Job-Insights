bab7a0e7d72ca55507979de345546ca653a23883
Mauro <MauroMarchesan88@gmail.com>
2022-10-21 11:01:08 -0300

    Requisito 11 completo, alteracao no rq 10

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    external_call = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+
+    "Verifica que a chave 'title' esteja"
+    assert external_call[0].get('title')
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,5 +2,5 @@ from src.counter import count_ocurrences
 
 
 def test_counter():
-    'Para um número ímpar a função deve retornar o valor True'
+    'Para a palavra work deve retornar 14174 instancias'
     assert count_ocurrences('src/jobs.csv', 'work') == 14174
