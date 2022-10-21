f3fe243fcea47e549c9fa913e85ce2d2062af0f2
Mauro <MauroMarchesan88@gmail.com>
2022-10-21 09:50:37 -0300

    Requisito 10 completo

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    'Para um número ímpar a função deve retornar o valor True'
+    assert count_ocurrences('src/jobs.csv', 'work') == 14174
